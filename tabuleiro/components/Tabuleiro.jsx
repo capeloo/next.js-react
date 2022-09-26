3dba7e93086ae73c38907b914fba873fdd1ff5f0
Caio Henrique Capêlo <108996602+capeloo@users.noreply.github.com>
2022-09-25 22:39:10 -0300

    project tabuleiro

---
+++ tabuleiro/components/Tabuleiro.jsx
@@ -0,0 +1,16 @@
+import Linha from "../components/Linha"
+
+export default function Tabuleiro(){
+    return (
+    <div>
+        <Linha />
+        <Linha preta/>
+        <Linha />
+        <Linha preta/>
+        <Linha />
+        <Linha preta/>
+        <Linha />
+        <Linha preta/>
+    </div>
+    )
+}
